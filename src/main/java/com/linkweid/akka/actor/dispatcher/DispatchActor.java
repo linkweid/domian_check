fdd3bc8c6e1556941b831b96809508a0e37ecbf8
lichuanshun <1058688398@qq.com>
2017-09-07 11:17:05 +0800

    1: init

---
+++ src/main/java/com/linkweid/akka/actor/dispatcher/DispatchActor.java
@@ -0,0 +1,17 @@
+package com.linkweid.akka.actor.dispatcher;
+
+import akka.actor.AbstractActor;
+
+/**
+ *
+ * 分发actor
+ * Created by lichuanshun on 2017/9/7.
+ */
+public class DispatchActor extends AbstractActor {
+
+
+    @Override
+    public Receive createReceive() {
+        return null;
+    }
+}
